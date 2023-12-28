e1f70e059aa1e6c1e2fdb83d0cd86b278e5766f0
Guillaume Malek <guillaume.malek@outlook.fr>
2023-12-28 14:02:28 +0100

    feat: add JWT

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,28 @@
 import 'dotenv/config'
-import fastify, { FastifyListenOptions } from 'fastify'
-import cors from '@fastify/cors'
+import { fastify, FastifyListenOptions, FastifyReply, FastifyRequest } from 'fastify'
+import { fastifyJwt } from '@fastify/jwt'
+import { fastifyCors } from '@fastify/cors'
 import mongoose from 'mongoose'
 import { routes } from './routes'
 
-if(!process.env.DATABASE_URI) throw new Error('DATABASE_URI is not defined')
-if(!process.env.FASTIFY_PORT) throw new Error('FASTIFY_PORT is not defined')
+if (!process.env.DATABASE_URI) throw new Error('DATABASE_URI is not defined')
+if (!process.env.FASTIFY_PORT) throw new Error('FASTIFY_PORT is not defined')
+if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined')
 
-const server = fastify({ logger: true })
-void server.register(cors, {})
-void server.register(routes)
-const start = async () => {
-  server.listen({port: process.env.FASTIFY_PORT, host:'0.0.0.0'} as FastifyListenOptions).then(() => {
-      console.log(`\n API started on http://localhost:${process.env.FASTIFY_PORT} \n`)
-      return
-    })
-    .catch((error: Error) => {
-      server.log.error(error.message)
-      throw new Error('Server error')
-    })
-}
+export const server = fastify({ logger: true })
+  .register(fastifyCors, { origin: process.env.CORS_ORIGINS?.split(';')} )
+  .register(routes)
+  .register(fastifyJwt, { secret: process.env.JWT_SECRET })
+  .addHook('onRequest', (request, _res, next) => { request.jwt = server.jwt; return next() })
+  .decorate('authenticate', async (request: FastifyRequest, reply: FastifyReply) => await request.jwtVerify().catch((error) => reply.send(error)))
+
+// graceful shutdown
+const listeners = ['SIGINT', 'SIGTERM']
+listeners.forEach((signal) => process.on(signal, async () => { await server.close(); process.exit(0) }))
+
 mongoose.connect(process.env.DATABASE_URI)
-  .then(() => {
-    console.log('connected to DB')
-    return
-  })
-  .catch((error: Error) => {
-    console.error(error.message)
-    throw new Error('database error')
-  })
+  .then(() => server.log.info('connected to database'))
+  .catch((error) => server.log.error(`database ${error.message}`))
 
-start()
+server.listen({ port: +process.env.FASTIFY_PORT, host: '0.0.0.0' } as FastifyListenOptions)
+  .catch((error: Error) => server.log.error(error.message))
