45ed9ced659b3183ce95049f2f4a82d440407ae1
Guillaume Malek <guillaume.malek@outlook.fr>
2023-12-28 12:22:57 +0100

    fix: add fastify request types

--- src/controllers/program.controller.ts
+++ src/controllers/program.controller.ts
@@ -1,13 +1,7 @@
 import { IProgram } from '../models';
-import { FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { programService } from '../services'
-
-interface FastifyRequest {
-  body: any
-  params: {
-    id: string
-  }
-}
+import { ParamsType } from '../utils'
 
 export const programController = {
   getAll: async (_request: FastifyRequest, reply: FastifyReply): Promise<void> => {
@@ -15,7 +9,7 @@ export const programController = {
       .then(programs => reply.type('application/json').code(200).send(programs))
       .catch(error => reply.type('application/json').code(400).send({ errorMessage: error.message }))
   },
-  get: async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+  get: async (request: FastifyRequest<{ Params: ParamsType }>, reply: FastifyReply): Promise<void> => {
     await programService.get(request.params.id)
       .then(program => {
         if (program) return reply.type('application/json').code(200).send(program)
@@ -23,13 +17,13 @@ export const programController = {
       })
       .catch(error => reply.type('application/json').code(400).send({ errorMessage: error.message }))
   },
-  save: async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+  save: async (request: FastifyRequest<{ Body: string }>, reply: FastifyReply): Promise<void> => {
     const program = JSON.parse(request.body) as IProgram
     await programService.upsert(program)
       .then(programs => reply.type('application/json').code(200).send(programs))
       .catch(error => reply.type('application/json').code(400).send({ errorMessage: error.message }))
   },
-  delete: async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+  delete: async (request: FastifyRequest<{ Params: ParamsType }>, reply: FastifyReply): Promise<void> => {
     await programService.delete(request.params.id)
       .then(() => reply.type('application/json').code(200).send({}))
       .catch(error => reply.type('application/json').code(401).send({ errorMessage: error.message }))
