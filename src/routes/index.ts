adeeb8882d35ee20a6b7f94adf705e9e920dfe55
Guillaume Malek <guillaume.malek@outlook.fr>
2023-12-28 14:05:44 +0100

    feat: add authentication service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 export { activityRoutes } from './activity.route'
 export { exerciseRoutes } from './exercise.route'
 export { programRoutes } from './program.route'
+export { authenticationRoutes } from './authentication.route'
 
-import { exerciseRoutes, programRoutes, activityRoutes } from '.';
+import { exerciseRoutes, programRoutes, activityRoutes, authenticationRoutes } from '.'
 import fp from 'fastify-plugin'
 
-const processRoutes = [activityRoutes, exerciseRoutes, programRoutes]
+const processRoutes = [activityRoutes, exerciseRoutes, programRoutes, authenticationRoutes]
 
 export const routes = fp(async (server) => {
   for (const route of processRoutes) server.register(route)
